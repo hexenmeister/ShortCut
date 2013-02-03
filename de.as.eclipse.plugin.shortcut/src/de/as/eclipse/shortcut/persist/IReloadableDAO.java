ce85e3bb9f5593b3352f48dd4e050828c26c6690
Hexenmeister <{mail}>
2013-02-04 00:05:46 +0100

    Kommentar

--- de.as.eclipse.plugin.shortcut/src/de/as/eclipse/shortcut/persist/IReloadableDAO.java
+++ de.as.eclipse.plugin.shortcut/src/de/as/eclipse/shortcut/persist/IReloadableDAO.java
@@ -3,8 +3,8 @@ package de.as.eclipse.shortcut.persist;
 /**
  * Dient als Marker für DAO-Klassen, die ihre Parameter (als ein String) ausgeben können
  * und mit diesem eine neue, gleichwertige Instanz erstellt werden kann.
- * Für die Wiederinitialisierung wird der Config-String an ein entsprechender Constuctor
- * übergeben (muss vorhanden sein).
+ * Für die Wiederinitialisierung wird der Config-String an einen entsprechender Constuctor
+ * übergeben (dieser muss vorhanden sein).
  *
  * @author Alexander Schulz
  * Date: 21.11.2012
@@ -12,7 +12,7 @@ package de.as.eclipse.shortcut.persist;
 public interface IReloadableDAO {
 
     /**
-     * Liefert String mit Konfigurationsparametern, mit denen eine gleichwertige Instanz (wieder) erstellt werden kann.
+     * Liefert String mit Konfigurationsparametern, mit denen eine gleichwertige Instanz (wieder-) erstellt werden kann.
      * @return Konfigurationsparameter.
      */
     public abstract String getConfigString();
